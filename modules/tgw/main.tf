42feb7ed05a61d20bd6cee1fca16b9d082077100
Seun Ogunbiyi <slimdat000@yahoo.com>
2023-01-29 12:53:14 +0000

    add

--- modules/tgw/main.tf
+++ modules/tgw/main.tf
@@ -1,135 +1,177 @@
-provider "aws" {
-  alias      = "tgw"
-  access_key = var.AWS_ACCESS_KEY_ID_TGW
-  secret_key = var.AWS_SECRET_ACCESS_KEY_TGW
-  token      = var.AWS_SESSION_TOKEN_TGW
-  region     = var.region
-}
+# provider "aws" {
+#   alias      = "tgw"
+#   access_key = var.AWS_ACCESS_KEY_ID_TGW
+#   secret_key = var.AWS_SECRET_ACCESS_KEY_TGW
+#   token      = var.AWS_SESSION_TOKEN_TGW
+#   region     = var.region
+# }
 
 locals {
-  tgw_state = {
-    outbound = "s01.env:/prod/s01.env.outbound/s01.env.transitgatewayprod.TransitGateway_state.tfstate"
-  }[var.tgw_environment]
-}
+  # List of maps with key and route values
+  vpc_attachments_with_routes = chunklist(flatten([
+    for k, v in var.vpc_attachments : setproduct([{ key = k }], v.tgw_routes) if var.create_tgw && can(v.tgw_routes)
+  ]), 2)
 
-data "terraform_remote_state" "tgw_state" {
-  backend = "s3"
-  config = {
-    bucket     = "tf.remote.state"
-    key        = local.tgw_state
-    region     = "eu-west-2"
-    encrypt    = true
-    access_key = var.AWS_ACCESS_KEY_ID_TGW
-    secret_key = var.AWS_SECRET_ACCESS_KEY_TGW
-    token      = var.AWS_SESSION_TOKEN_TGW
-  }
-}
-
-data "aws_ec2_transit_gateway_route_table" "cust" {
-  provider = aws.tgw
-  filter {
-    name   = "tag:Name"
-    values = ["prod-outbound-cust"]
-  }
-
-  filter {
-    name   = "transit-gateway-id"
-    values = [local.tgwid]
-  }
-}
-
-locals {
-  tgwid      = data.terraform_remote_state.tgw_state.outputs.tgw_id
-  sharedrtid = data.terraform_remote_state.tgw_state.outputs.shared_transit_rt_id
-  custrtid   = data.aws_ec2_transit_gateway_route_table.cust.id
-}
-
-resource "aws_ram_principal_association" "transit_principal_association" {
-  provider           = aws.tgw
-  principal          = data.aws_caller_identity.current.account_id
-  resource_share_arn = data.terraform_remote_state.tgw_state.outputs.aws_ram_resource_share_transit_gw_arn
-}
-
-resource "aws_ec2_transit_gateway_vpc_attachment" "publishing_transit_attachment" {
-  vpc_id                                          = aws_vpc.vpc.id
-  transit_gateway_id                              = local.tgwid
-  subnet_ids                                      = [element(aws_subnet.pv1_sub.*.id, 0), element(aws_subnet.pv1_sub.*.id, 1)]
-  transit_gateway_default_route_table_association = false
-  transit_gateway_default_route_table_propagation = false
-  appliance_mode_support                          = "enable"
-
-  depends_on = [aws_ram_principal_association.transit_principal_association]
-
-  tags = merge(
-    {
-      "Name"    = "ice-${var.project}-${terraform.workspace}-tgw-attach"
-      "Owner"   = var.owner
-      "Project" = join("-", ["ice", var.project, terraform.workspace])
-    },
+  tgw_default_route_table_tags_merged = merge(
+    var.tags,
+    { Name = var.name },
+    var.tgw_default_route_table_tags,
   )
 
-  # Ignore changes to the TGW attachments default route table settings.  These are hardcoded to true in the terraform source code.
-  lifecycle {
-    ignore_changes = [transit_gateway_default_route_table_association, transit_gateway_default_route_table_propagation]
+  vpc_route_table_destination_cidr = flatten([
+    for k, v in var.vpc_attachments : [
+      for rtb_id in try(v.vpc_route_table_ids, []) : {
+        rtb_id = rtb_id
+        cidr   = v.tgw_destination_cidr
+      }
+    ]
+  ])
+}
+
+# Transit Gateway
+
+resource "aws_ec2_transit_gateway" "this" {
+  count = var.create_tgw ? 1 : 0
+
+  description                     = coalesce(var.description, var.name)
+  amazon_side_asn                 = var.amazon_side_asn
+  default_route_table_association = var.enable_default_route_table_association ? "enable" : "disable"
+  default_route_table_propagation = var.enable_default_route_table_propagation ? "enable" : "disable"
+  auto_accept_shared_attachments  = var.enable_auto_accept_shared_attachments ? "enable" : "disable"
+  multicast_support               = var.enable_mutlicast_support ? "enable" : "disable"
+  vpn_ecmp_support                = var.enable_vpn_ecmp_support ? "enable" : "disable"
+  dns_support                     = var.enable_dns_support ? "enable" : "disable"
+  transit_gateway_cidr_blocks     = var.transit_gateway_cidr_blocks
+
+  timeouts {
+    create = try(var.timeouts.create, null)
+    update = try(var.timeouts.update, null)
+    delete = try(var.timeouts.delete, null)
   }
 
-  provisioner "local-exec" {
-    command = <<EOS
-AWS_DEFAULT_REGION=${var.region} aws ec2 create-tags \
---resources ${aws_ec2_transit_gateway_vpc_attachment.publishing_transit_attachment.id} \
---tags \
-Key=Name,Value=\"ice-${var.project}-${terraform.workspace}-tgw-attach\" \
-Key=Owner,Value=\"${var.owner}\" \
-Key=Project,Value=\"${join("-", ["ice", var.project, terraform.workspace])}\" \
-Key=Purpose,Value=\"Publishing\" \
-Key=Environment,Value=\"${terraform.workspace}\"
-EOS
+  tags = merge(
+    var.tags,
+    { Name = var.name },
+    var.tgw_tags,
+  )
+}
 
+resource "aws_ec2_tag" "this" {
+  for_each = { for k, v in local.tgw_default_route_table_tags_merged : k => v if var.create_tgw && var.enable_default_route_table_association }
 
-    environment = {
-      AWS_ACCESS_KEY_ID     = var.AWS_ACCESS_KEY_ID_TGW
-      AWS_SECRET_ACCESS_KEY = var.AWS_SECRET_ACCESS_KEY_TGW
-      AWS_SESSION_TOKEN     = var.AWS_SESSION_TOKEN_TGW
-    }
+  resource_id = aws_ec2_transit_gateway.this[0].association_default_route_table_id
+  key         = each.key
+  value       = each.value
+}
+
+# VPC Attachment
+
+resource "aws_ec2_transit_gateway_vpc_attachment" "this" {
+  for_each = var.vpc_attachments
+
+  transit_gateway_id = var.create_tgw ? aws_ec2_transit_gateway.this[0].id : each.value.tgw_id
+  vpc_id             = each.value.vpc_id
+  subnet_ids         = each.value.subnet_ids
+
+  dns_support                                     = try(each.value.dns_support, true) ? "enable" : "disable"
+  ipv6_support                                    = try(each.value.ipv6_support, false) ? "enable" : "disable"
+  appliance_mode_support                          = try(each.value.appliance_mode_support, false) ? "enable" : "disable"
+  transit_gateway_default_route_table_association = try(each.value.transit_gateway_default_route_table_association, true)
+  transit_gateway_default_route_table_propagation = try(each.value.transit_gateway_default_route_table_propagation, true)
+
+  tags = merge(
+    var.tags,
+    { Name = var.name },
+    var.tgw_vpc_attachment_tags,
+  )
+}
+
+# Route Table / Routes
+
+resource "aws_ec2_transit_gateway_route_table" "this" {
+  count = var.create_tgw ? 1 : 0
+
+  transit_gateway_id = aws_ec2_transit_gateway.this[0].id
+
+  tags = merge(
+    var.tags,
+    { Name = var.name },
+    var.tgw_route_table_tags,
+  )
+}
+
+resource "aws_ec2_transit_gateway_route" "this" {
+  count = length(local.vpc_attachments_with_routes)
+
+  destination_cidr_block = local.vpc_attachments_with_routes[count.index][1].destination_cidr_block
+  blackhole              = try(local.vpc_attachments_with_routes[count.index][1].blackhole, null)
+
+  transit_gateway_route_table_id = var.create_tgw ? aws_ec2_transit_gateway_route_table.this[0].id : var.transit_gateway_route_table_id
+  transit_gateway_attachment_id  = tobool(try(local.vpc_attachments_with_routes[count.index][1].blackhole, false)) == false ? aws_ec2_transit_gateway_vpc_attachment.this[local.vpc_attachments_with_routes[count.index][0].key].id : null
+}
+
+resource "aws_route" "this" {
+  for_each = { for x in local.vpc_route_table_destination_cidr : x.rtb_id => x.cidr }
+
+  route_table_id         = each.key
+  destination_cidr_block = each.value
+  transit_gateway_id     = aws_ec2_transit_gateway.this[0].id
+}
+
+resource "aws_ec2_transit_gateway_route_table_association" "this" {
+  for_each = {
+    for k, v in var.vpc_attachments : k => v if var.create_tgw && try(v.transit_gateway_default_route_table_association, true) != true
   }
+
+  # Create association if it was not set already by aws_ec2_transit_gateway_vpc_attachment resource
+  transit_gateway_attachment_id  = aws_ec2_transit_gateway_vpc_attachment.this[each.key].id
+  transit_gateway_route_table_id = var.create_tgw ? aws_ec2_transit_gateway_route_table.this[0].id : try(each.value.transit_gateway_route_table_id, var.transit_gateway_route_table_id)
 }
 
-resource "null_resource" "delayagain" {
-  depends_on = [aws_ec2_transit_gateway_vpc_attachment.publishing_transit_attachment]
-  provisioner "local-exec" {
-    command = "sleep 60"
+resource "aws_ec2_transit_gateway_route_table_propagation" "this" {
+  for_each = {
+    for k, v in var.vpc_attachments : k => v if var.create_tgw && try(v.transit_gateway_default_route_table_propagation, true) != true
   }
+
+ # Create association if it was not set already by aws_ec2_transit_gateway_vpc_attachment resource
+  transit_gateway_attachment_id  = aws_ec2_transit_gateway_vpc_attachment.this[each.key].id
+  transit_gateway_route_table_id = var.create_tgw ? aws_ec2_transit_gateway_route_table.this[0].id : try(each.value.transit_gateway_route_table_id, var.transit_gateway_route_table_id)
 }
 
-# Propagate this route to the shared service route table
-resource "aws_ec2_transit_gateway_route_table_propagation" "shared_rt_propagation" {
-  provider                       = aws.tgw
-  transit_gateway_attachment_id  = aws_ec2_transit_gateway_vpc_attachment.publishing_transit_attachment.id
-  transit_gateway_route_table_id = local.sharedrtid
+# Resource Access Manager
+
+esource "aws_ram_resource_share" "this" {
+  count = var.create_tgw && var.share_tgw ? 1 : 0
+
+  name                      = coalesce(var.ram_name, var.name)
+  allow_external_principals = var.ram_allow_external_principals
+
+  tags = merge(
+    var.tags,
+    { Name = coalesce(var.ram_name, var.name) },
+    var.ram_tags,
+  )
+}
+
+resource "aws_ram_resource_association" "this" {
+  count = var.create_tgw && var.share_tgw ? 1 : 0
+
+  resource_arn       = aws_ec2_transit_gateway.this[0].arn
+  resource_share_arn = aws_ram_resource_share.this[0].id
+}
+
+resource "aws_ram_principal_association" "this" {
+  count = var.create_tgw && var.share_tgw ? length(var.ram_principals) : 0
+
+  principal          = var.ram_principals[count.index]
+  resource_share_arn = aws_ram_resource_share.this[0].arn
+}
+
+resource "aws_ram_resource_share_accepter" "this" {
+  count = !var.create_tgw && var.share_tgw ? 1 : 0
+
+  share_arn = var.ram_resource_share_arn
 }
 
 
-resource "aws_ec2_transit_gateway_route_table_association" "outbound_attachment_transit_rt_association" {
-  provider                       = aws.tgw
-  transit_gateway_attachment_id  = aws_ec2_transit_gateway_vpc_attachment.publishing_transit_attachment.id
-  transit_gateway_route_table_id = local.custrtid
-  depends_on                     = [null_resource.delayagain]
-}
 
-# Add the route to send all traffic back across the transit gateway
-resource "aws_route" "transit_rt" {
-  route_table_id         = aws_route_table.private.id
-  destination_cidr_block = "10.0.0.0/8"
-  transit_gateway_id     = data.terraform_remote_state.tgw_state.outputs.tgw_id
-
-  depends_on = [aws_ram_principal_association.transit_principal_association]
-}
-
-#create a route for VMWare SDDC
-resource "aws_route" "sddc_rt" {
-  count                  = local.is_prod ? 1 : 0 # only in prod
-  route_table_id         = aws_route_table.private.id
-  destination_cidr_block = var.artic_address_range
-  transit_gateway_id     = var.artic_tgw_id
-  depends_on             = [aws_ram_principal_association.transit_principal_association]
-}
