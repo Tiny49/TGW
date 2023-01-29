d8cbf99510355fe1ce914e5a5fe2408332535272
Seun Ogunbiyi <slimdat000@yahoo.com>
2023-01-29 09:49:43 +0000

    added blocks to multiple .tfs

--- provider.tf
+++ provider.tf
@@ -1,20 +1,7 @@
-terraform {
-  required_version = ">= 1.2.3"
-
-  backend "s3" {
-  }
-
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.0"
-    }
-    null = {
-      source = "hashicorp/null"
-    }
-  }
-}
-
 provider "aws" {
-  region  = var.region
+  alias      = "mpa"
+  access_key = var.AWS_ACCESS_KEY_ID_MPA
+  secret_key = var.AWS_SECRET_ACCESS_KEY_MPA
+  token      = var.AWS_SESSION_TOKEN_MPA
+  region     = var.region
 }
